8d3f4006418e8582c6152dcd25cf5961d4a51be8
Jirobe <38680274+Jirobe@users.noreply.github.com>
2019-03-30 15:52:53 +0900

    Sample_DependencyService

---
+++ Sample_DependencyService/Sample/Sample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Sample.Services;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Sample.ViewModels
+{
+    class MainPageViewModel : BaseViewModel
+    {
+        //ADD Start
+        private string uRIText;
+
+        public string URIText
+        {
+            get { return uRIText; }
+            set { SetProperty(ref uRIText, value); }
+        }
+        //ADD End
+
+        //MOD Start
+        //public ICommand DeviceOpenUriCommand => new Command(() => Device.OpenUri(new Uri(@"https://www.google.com/")));
+
+        //public ICommand DependencyServiceCommand => new Command(() =>
+        //{
+        //    var openUriService = DependencyService.Get<IOpenUriService>();
+        //    openUriService.OpenUri(@"https://www.google.com/");
+        //});
+
+        public ICommand DeviceOpenUriCommand => new Command(() => Device.OpenUri(new Uri(URIText)));
+
+        public ICommand DependencyServiceCommand => new Command(() =>
+        {
+            var openUriService = DependencyService.Get<IOpenUriService>();
+            
+            openUriService.OpenUri(URIText);
+        });
+        //MOD End
+    }
+}
